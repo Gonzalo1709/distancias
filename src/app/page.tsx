984bd2366e45435d5f5ec5cf870988677e3b585e
FlaviaManuico <83974322+FlaviaManuico@users.noreply.github.com>
2024-06-10 22:15:31 -0500

    Menu con 2 opciones: comparar 2 o 3 ciudades

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,21 +10,22 @@ export interface dataType {
 export default function Home() {
   const [data1, setData1] = useState<dataType | null>(null);
   const [data2, setData2] = useState<dataType | null>(null);
-
+  const [data3, setData3] = useState<dataType | null>(null);
+  const [option, setOption] = useState<string>("twoCities");
   const [result, setResult] = useState<string>("");
 
   const handleSubmit = (event: any) => {
-    if (!data1 || !data2) {
-      alert("Please fill all the fields");
-      return;
-    }
-    const params = new URLSearchParams({
-      country1: data1.country,
-      city1: data1.city,
-      country2: data2.country,
-      city2: data2.city,
-    });
     event.preventDefault();
+    const params = new URLSearchParams({
+      country1: data1!.country,
+      city1: data1!.city,
+      country2: data2!.country,
+      city2: data2!.city,
+      country3: data3 ? data3.country : '',
+      city3: data3 ? data3.city : '',
+      option,
+    });
+
     fetch(`http://127.0.0.1:5000/?${params.toString()}`, {
       method: "GET",
       headers: {
@@ -33,41 +34,61 @@ export default function Home() {
     })
       .then((response) => response.json())
       .then((data) => {
-        setResult(data.distance);
+        setResult(data.closest_distance);
       })
       .catch((error) => {
         console.error("Error:", error);
       });
   };
-  if (result !== "") {
-    return (
-      <div className="flex flex-col items-center justify-evenly h-[100vh]">
-        <div>
-          <h1 className="text-2xl">La distancia entre las ciudades es: </h1>
-          <h2 className="text-4xl">{result} kilometros</h2>
-        </div>
-        <button
-          onClick={() => setResult("")}
-          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-        >
-          Go back
-        </button>
-      </div>
-    );
-  }
+  
   return (
     <form
       className="flex flex-col items-center justify-evenly h-[100vh]"
       onSubmit={handleSubmit}
     >
+      <div className="flex justify-around items-center">
+        <label>
+          <input
+            type="radio"
+            name="option"
+            value="twoCities"
+            checked={option === "twoCities"}
+            onChange={() => setOption("twoCities")}
+          />
+          Calcular distancia entre dos ciudades
+        </label>
+        <label>
+          <input
+            type="radio"
+            name="option"
+            value="threeCities"
+            checked={option === "threeCities"}
+            onChange={() => setOption("threeCities")}
+          />
+          Encontrar las dos ciudades más cercanas
+        </label>
+      </div>
       <CountryCitySelector id="1" setData={setData1} />
       <CountryCitySelector id="2" setData={setData2} />
+      {option === "threeCities" && (
+        <CountryCitySelector id="3" setData={setData3} />
+      )}
       <button
         type="submit"
         className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
       >
         Submit
       </button>
+      {result && (
+        <div className="flex flex-col items-center justify-evenly">
+          <h1 className="text-2xl">{option === "twoCities" ? "La distancia entre las ciudades es:" : "Las ciudades más cercanas son:"}</h1>
+          {option === "twoCities" ? (
+          <h2 className="text-4xl">{result} kilómetros</h2>
+          ) : (
+          <h2 className="text-4xl">{result}</h2>
+          )}
+        </div>
+      )}
     </form>
   );
 }
